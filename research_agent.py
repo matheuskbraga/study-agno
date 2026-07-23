d023bd252a6651e43245ab5bd8458c215b449e6d
matheus desktop <matheuskbraga9@gmail.com>
2026-07-23 13:11:25 -0300

    Desenvolvendo funcionalidades para o Agente de IA.

--- research_agent.py
+++ research_agent.py
@@ -2,18 +2,26 @@ from agno.agent import Agent
 from agno.models.groq import Groq
 from agno.tools.tavily import TavilyTools
 
+
+# 1. Carregando as variáveis de ambiente
 from dotenv import load_dotenv
 load_dotenv()
 
+
+# 2. Criando o agente
 agent = Agent(
     model=Groq(id="llama-3.3-70b-versatile"),
-    tools=[TavilyTools()],
+    tools=[
+        TavilyTools(),
+        ],
 )
 
+
+# 3. Resposta do agente
 agent.print_response(
     """
     Use a ferramenta Tavily para pesquisar quais são os melhores
     investimentos para guardar um salário mensal no Brasil atualmente.
-    Considere segurança, liquidez e rentabilidade.
+    Considere segurança, liquidez e rentabilidade. Retorne em um parágrafo simples e de fácil entendimento.
     """
 )
---
+++ track_time.py
@@ -0,0 +1,41 @@
+from agno.agent import Agent
+from agno.models.groq import Groq
+from agno.tools.tavily import TavilyTools
+
+
+# 1. Carregando as variáveis de ambiente
+from dotenv import load_dotenv
+load_dotenv()
+
+
+# 2. Criando a função para calcular a temperatura em Fahrenheit
+def celsius_to_fh(temperatura_celcius: float):
+    """
+    Converte a temperatura de Celsius para Fahrenheit.
+
+    Args:
+        temperatura_celcius (float): Temperatura em Celsius.
+
+    Returns:
+        float: Temperatura em Fahrenheit.
+    """
+    return (temperatura_celcius * 9/5) + 32
+
+
+# 3. Criando o agente
+agent = Agent(
+    model=Groq(id="llama-3.3-70b-versatile"),
+    tools=[
+        TavilyTools(),
+        celsius_to_fh, # 4. Passando a função criada para o agente
+        ],
+)
+
+
+# 5. Resposta do agente
+agent.print_response(
+    """
+    Use suas ferramentas para pesquisar a temperatura hoje em Santa Maria, Rio Grande do Sul, em Fahrenheit.
+    Retorne em um parágrafo simples e de fácil entendimento.
+    """
+)
